c48bc0009efc2b74915cdba8a9ea0f7a308e9d4e
bluhar <413128818@qq.com>
2014-11-12 23:29:51 +0800

    copy wechat

---
+++ WeChatSample/src/com/example/wechatsample/ChatFragment.java
@@ -0,0 +1,47 @@
+package com.example.wechatsample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.FrameLayout.LayoutParams;
+
+/**
+ * 聊天Fragment的界面
+ * 
+ * http://blog.csdn.net/guolin_blog/article/details/26365683
+ * 
+ * @author guolin
+ */
+public class ChatFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+	    
+	    //这里完全可以定义一个fragment layout xml, 我来把他下面的代码实现一下，通过XML来布局
+	    View view = inflater.inflate(R.layout.fragment_chat, container, false);
+	    
+	    return view;
+	    
+//		LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+//		FrameLayout fl = new FrameLayout(getActivity());
+//		fl.setLayoutParams(params);
+//		DisplayMetrics dm = getResources().getDisplayMetrics();
+//		final int margin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, dm);
+//		TextView v = new TextView(getActivity());
+//		params.setMargins(margin, margin, margin, margin);
+//		v.setLayoutParams(params);
+//		v.setLayoutParams(params);
+//		v.setGravity(Gravity.CENTER);
+//		v.setText("聊天界面");
+//		v.setTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, dm));
+//		fl.addView(v);
+//		return fl;
+	}
+}
